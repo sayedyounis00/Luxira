82f80a805c637693a22902216953f8ea7b6fe934
sayed younis <elsaydseif17@gmail.com>
2024-09-23 06:39:12 +0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -27,19 +27,4 @@ class Luxira extends StatelessWidget {
       ),
     );
   }
-@override
-Widget build(BuildContext context) {
-  return  ScreenUtilInit(
-    designSize: const Size(990.63, 2147.69),
-    minTextAdapt: true,
-    child: BlocProvider(
-      create: (context) => RegisterCubit(),
-      child: const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        onGenerateRoute: onGenerate,
-        initialRoute: AppRouter.loginPage,
-      ),
-    ),
-  );
-}
 }

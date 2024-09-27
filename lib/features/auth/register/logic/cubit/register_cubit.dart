cd913d097ae64955c035d2f467639e2ef0974538
yousefdewidar <ymahmoud1213@gmail.com>
2024-09-27 06:49:50 +0300

    fix var name done.

--- lib/features/auth/register/logic/cubit/register_cubit.dart
+++ lib/features/auth/register/logic/cubit/register_cubit.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:luxira/core/helper/networking/api_constants.dart';
+import 'package:luxira/core/helper/networking/api_services.dart';
 import 'package:luxira/features/auth/register/data/err_response.dart';
 import 'package:luxira/features/auth/register/data/new_user.dart';
 
@@ -13,14 +14,13 @@ part 'register_state.dart';
 class RegisterCubit extends Cubit<RegisterState> {
   RegisterCubit() : super(RegisterInitial());
   TextEditingController emailcont = TextEditingController();
-
-  //!
   final GoogleSignIn googleSignIn = GoogleSignIn(
     scopes: [
       'email',
       ApiConstants.regiserUsingGoogle,
     ],
   );
+
   void register(NewUser newUser) async {
     emit(RegisterLoading());
     emailcont.text = newUser.email;
@@ -32,7 +32,8 @@ class RegisterCubit extends Cubit<RegisterState> {
       'password': newUser.password,
     };
     try {
-      await Dio().post(ApiConstants.register, data: data);
+      await ApiServices().postMethod(data: data, url: ApiConstants.register);
+      // await Dio().post(ApiConstants.register, data: data);
       emit(RegisterSuccess());
     } on DioException catch (e) {
       log(e.response!.data.runtimeType.toString());
